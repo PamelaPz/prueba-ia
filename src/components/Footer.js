a9d3ea8a0fcca4df0ebd50c8d07f037f0aae684d
PamelaPz <jpamelapz02@gmail.com>
2021-06-11 13:39:55 -0500

    responsive hasta laptop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,15 @@ import imgVitalinea from '../assets/images/img-logo-footer.png';
 function Footer() {
   return (
     <div className="wp_footer">
-        <div className="wp_headerF">
+        <div className="wp_img1">
             <img src={imgDanone} alt="danone"/>
+        </div>
+        <div className="wp_img2">
             <img src={imgVitalinea} alt="vitalinea"/>
         </div>
         <div className="wp_infoFooter">
             <p className="p_terminos">
-                Términos y condiciones| Políticas de privacidad | Aviso de privacidad
+                <span>Términos y condiciones</span> | <span>Políticas de privacidad</span> | <span>Aviso de privacidad</span>
             </p>
             <p className="p_derechos">
                 Danone de México 2017 ©. Todos los derechos reservados 2017
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
 import logo from '../assets/images/img-logo-vitalinea.png';
 import fresa2 from '../assets/images/img-fresas-2.png';
 import fresa5 from '../assets/images/img-fresas-5.png';
+import fresa3 from '../assets/images/img-fresas-3.png';
+import facebok from '../assets/images/img-social-facebook.svg';
+import instagram from '../assets/images/img-social-instagram.svg';
+import youtube from '../assets/images/img-social-youtube.svg';
 
 function NavBar() {
   return (
@@ -18,12 +22,31 @@ function NavBar() {
                             <span></span>
                         </div>
                     </li>
+                    <div className="menu_show">
+                        <li>
+                            <a href="/">Nuestros productos</a>
+                        </li>
+                        <li>
+                            <a href="/">Disfruta cuidarte</a>
+                        </li>
+                        <li>
+                            <a href="/">Blog</a>
+                        </li>
+                        <li className="social_media">
+                            <ul>
+                                <li><img src={facebok} alt="social"/></li>
+                                <li><img src={instagram} alt="social"/></li>
+                                <li><img src={youtube} alt="social"/></li>
+                            </ul>
+                        </li>
+                    </div>
                 </ul>
             </nav>
         </div>
         <div className="wp_fresasAbsolute">
             <img src={fresa2} alt="fresa2" id="fresa2"/>
             <img src={fresa5} alt="fresa5" id="fresa5"/>
+            <img src={fresa3} alt="fresa3" id="fresa3"/>
         </div>
     </div>
   );
